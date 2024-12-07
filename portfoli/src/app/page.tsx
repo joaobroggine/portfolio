11a2802613c1d21f710da24199f66f8c3a2ab8d7
joaobroggine <joaovitorbrogginelopes@gmail.com>
2024-12-07 19:33:09 -0300

    Feita a primeira parte da homepage do site, e criando rfc de outras páginas.

--- portfoli/src/app/page.tsx
+++ portfoli/src/app/page.tsx
@@ -5,32 +5,62 @@ import Header from '@/components/header'
 export default function page() {
   return (
     <div className='text-white'>
-      <Header/>
-      <main className='font-staatliches'>
+      <Header />
+      <main className='font-staatliches pt-10'>
         <div className='mt-16 bg-white w-screen h-96 flex'>
           <div className='w-1/2 h-full bg-[#87cefa] flex flex-col items-center justify-center'>
-            <h1 className='text-7xl w-3/4'
-            >Olá, Sou Broggine, programador em 
-            <span className="animate-color-change"> NEXT.JS14</span></h1>
+            <h1 className='text-7xl w-3/4'>
+              Olá, Sou Broggine, programador em
+              <span className="animate-color-change"> NEXT.JS14</span>
+            </h1>
             <div className='flex items-center w-full pl-20 space-x-2'>
               <a href="https://github.com/joaobroggine" target='_blank'>
-                <Image className='hover:scale-110 duration-300'
-                src="/img/githubicon.webp" alt='Github Icon' width={50} height={50}></Image>
+                <Image className='hover:scale-110 duration-300' src="/img/githubicon.webp" alt='Github Icon' width={50} height={50} />
               </a>
               <a href="https://www.linkedin.com/in/jo%C3%A3o-broggine-5b92a02b2/?trk=opento_sprofile_goalscard" target='_blank'>
-                <Image className='hover:scale-110 duration-300'
-                src="/img/linkedin.png" alt='Linkedin Icon' width={42} height={42}></Image>
+                <Image className='hover:scale-110 duration-300' src="/img/linkedin.png" alt='Linkedin Icon' width={42} height={42} />
               </a>
             </div>
           </div>
           <div className='w-1/2 h-full bg-[#87cefa] flex items-center justify-center'>
-            <Image className='rounded-full hover:scale-110 duration-500 shadow-xl'
-             src="/img/me.png" alt='Broggine' width={400} height={400}/>
+            <Image className='rounded-full hover:scale-110 duration-500 shadow-xl' src="/img/me.png" alt='Broggine' width={400} height={400} />
           </div>
         </div>
       </main>
+      <section className="mt-16 flex justify-center items-center flex-wrap gap-8">
+        <div className="flex items-center justify-center p-4 rounded-xl border-2 border-gray-200 bg-white shadow-xl hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-110">
+          <Image src="/img/html.webp" alt="HTML Icon" width={40} height={40} />
+        </div>
+        <div className="flex items-center justify-center p-4 rounded-xl border-2 border-gray-200 bg-white shadow-xl hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-110">
+          <Image src="/img/css.png" alt="CSS Icon" width={40} height={40} />
+        </div>
+        <div className="flex items-center justify-center p-4 rounded-xl border-2 border-gray-200 bg-white shadow-xl hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-110">
+          <Image src="/img/javascript.webp" alt="JavaScript Icon" width={40} height={40} />
+        </div>
+        <div className="flex items-center justify-center p-4 rounded-xl border-2 border-gray-200 bg-white shadow-xl hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-110">
+          <Image src="/img/tailwind.webp" alt="Tailwind Icon" width={40} height={40} />
+        </div>
+        <div className="flex items-center justify-center p-4 rounded-xl border-2 border-gray-200 bg-white shadow-xl hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-110">
+          <Image src="/img/react.webp" alt="React Icon" width={40} height={40} />
+        </div>
+        <div className="flex items-center justify-center p-4 rounded-xl border-2 border-gray-200 bg-white shadow-xl hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-110">
+          <Image src="/img/nextjs.png" alt="NextJS Icon" width={40} height={40} />
+        </div>
+        <div className="flex items-center justify-center p-4 rounded-xl border-2 border-gray-200 bg-white shadow-xl hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-110">
+          <Image src="/img/python.webp" alt="Python Icon" width={40} height={40} />
+        </div>
+        <div className="flex items-center justify-center p-4 rounded-xl border-2 border-gray-200 bg-white shadow-xl hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-110">
+          <Image src="/img/java.png" alt="Java Icon" width={40} height={40} />
+        </div>
+        <div className="flex items-center justify-center p-4 rounded-xl border-2 border-gray-200 bg-white shadow-xl hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-110">
+          <Image src="/img/spring.png" alt="Spring Icon" width={40} height={40} />
+        </div>
+        <div className="flex items-center justify-center p-4 rounded-xl border-2 border-gray-200 bg-white shadow-xl hover:shadow-2xl transition duration-300 ease-in-out transform hover:scale-110">
+          <Image src="/img/oracledb.png" alt="OracleDB Icon" width={40} height={40} />
+        </div>
+      </section>
       <section>
-
+        <p></p>
       </section>
     </div>
   )
--- portfoli/src/components/header.tsx
+++ portfoli/src/components/header.tsx
@@ -3,15 +3,25 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className=''>
+    <div className='bg-[#87cefa] shadow-md h-16 fixed top-0 w-full z-50'>
       <nav className='flex justify-center space-x-10 pt-5 mr-16 font-dosis font-bold text-xl text-white'>
-        <ul>Idiomas</ul>
-        <ul><a href="/cv/curriculoprofissional.pdf"
-                    download
-            >Baixar Currículo</a></ul>
-        <ul><Link href="/">Home</Link></ul>
-        <ul>Linguagens</ul>
-        <ul>Sobre mim</ul>
+        <ul className='hover:scale-110 transform transition duration-300 ease-in-out hover:text-gray-800 hover:text-shadow-xl'>
+        <Link href="/idiomas">Idiomas</Link>
+        </ul>
+        <ul className='hover:scale-110 transform transition duration-300 ease-in-out hover:text-gray-800 hover:text-shadow-xl'>
+          <a href="/cv/curriculoprofissional.pdf" download>
+            Baixar Currículo
+          </a>
+        </ul>
+        <ul className='hover:scale-110 transform transition duration-300 ease-in-out hover:text-gray-800 hover:text-shadow-xl'>
+          <Link href="/">Home</Link>
+        </ul>
+        <ul className='hover:scale-110 transform transition duration-300 ease-in-out hover:text-gray-800 hover:text-shadow-xl'>
+        <Link href="/linguagens">Linguagens</Link>
+        </ul>
+        <ul className='hover:scale-110 transform transition duration-300 ease-in-out hover:text-gray-800 hover:text-shadow-xl'>
+        <Link href="/sobre">Sobre mim</Link>
+        </ul>
       </nav>
     </div>
   )
